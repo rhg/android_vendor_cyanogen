e85f0fba4144dccb8cd575159de4b428b1fd1d50
Ricardo Gomez <ricardo.gomez331@gmail.com>
2012-05-14 16:03:38 -0500

    Now builds for gelato
    Change-Id: Ic55fd16d3f75d1a56310cae571cffbe3b9a8d555

--- products/AndroidProducts.mk
+++ products/AndroidProducts.mk
@@ -1,4 +1,4 @@
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/cyanogen_generic.mk \
     $(LOCAL_DIR)/cyanogen_gelato_VM701.mk \
-    $(LOCAL_DIR)/cyanogen_gelato_L55C.mk
+    $(LOCAL_DIR)/cyanogen_gelato_LGL55C.mk
